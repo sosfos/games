266bf815309057ee0c268654aafb3ca0c54fe02a
sosfos <sosfos@126.com>
2016-03-19 21:52:34 +0800

    3

--- main.py
+++ main.py
@@ -13,35 +13,44 @@ from kivy.properties import ObjectProperty, NumericProperty, BooleanProperty,Str
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 
-Rows_For_First_Level = 3
+Rows_For_First_Level = 8
 
-class BButton(Button):    
+class BButton(Button):
     def __init__(self,box,**kwargs):
         super(BButton,self).__init__(**kwargs)
         self.box = box
         self.isMarking = False
-        
+
     def on_press(self):
         self.press()
-        
+
     def press(self):
         self.isMarking = self.box.root.status_bar.toggle_mark.state=='down'
-            
+
         if self.isMarking:
             if self.image.opacity == 1:
-                self.image.opacity = 0
-                self.box.state=0
+                self.MarkNormal()
             else:
-                self.image.opacity = 1
-                self.box.state=1
+                self.MarkB()
         else:
             self.box.Clear()
             self.box.state = -1
 
+    def MarkB(self):
+        self.image.opacity = 1
+        self.box.state=1
+
+    def MarkNormal(self):
+        self.image.opacity = 0
+        self.box.state=0
+
+class BLabel(Label):
+    pass
+
 class BBox(RelativeLayout):
-    def __init__(self,root,isBomb,**kwargs):
+    def __init__(self,root,**kwargs):
         super(BBox,self).__init__(**kwargs)
-        self.isBomb = isBomb
+        self.isBomb = False
         self.BNumber = 0
         self.root = root
         self.isClear = False
@@ -49,37 +58,35 @@ class BBox(RelativeLayout):
         self.col = 0
         self.state=0 # 0 means normal, 1 means marked as bomb, -1 means cleared already
         
-        self.blabel = Label()
+        self.blabel = BLabel()
         self.bbutton = BButton(box=self)
         
         self.add_widget(self.bbutton)
         
     def CalculateBNumber(self):
+        self.BNumber = 0
         if self.isBomb == False:
-            for i in range(self.row - 1, self.row + 1):
-                if i <= 0 or i > (self.root.level + Rows_For_First_Level):
-                    continue
-                for j in range(self.col - 1, self.col + 1):
-                    if j <= 0 or j > (self.root.level + Rows_For_First_Level):
-                        continue
-                    
-                    if i == self.row and j == self.col:
-                        continue
-                    
-                    if self.root.BBoxList[j + (i-1)*(self.root.level + Rows_For_First_Level) - 1].isBomb:
-                        self.BNumber += 1
-                        
+            self.BNumber += 1 if self.root.CheckBomb(self.row - 1,self.col - 1) else 0
+            self.BNumber += 1 if self.root.CheckBomb(self.row - 1,self.col) else 0
+            self.BNumber += 1 if self.root.CheckBomb(self.row - 1,self.col + 1) else 0
+            self.BNumber += 1 if self.root.CheckBomb(self.row,self.col - 1) else 0
+            self.BNumber += 1 if self.root.CheckBomb(self.row,self.col + 1) else 0
+            self.BNumber += 1 if self.root.CheckBomb(self.row + 1,self.col - 1) else 0
+            self.BNumber += 1 if self.root.CheckBomb(self.row + 1,self.col) else 0
+            self.BNumber += 1 if self.root.CheckBomb(self.row + 1,self.col + 1) else 0
+
             self.blabel.text = '{}'.format(self.BNumber) if self.BNumber > 0 else ''
         else:
             self.blabel.text='*'
-        
-                    
+    
+    
+
     
     def Clear(self,isShowAll=False):
         '''
         triggered when user click the button and think it's empty or has a number
         '''
-        if self.BNumber == -1:
+        if self.isBomb:
             '''
             A Bomb, game over
             '''
@@ -94,6 +101,7 @@ class BBox(RelativeLayout):
         '''
         Animation needed
         '''
+        self.root.ShowAll()
         pass
     
     def MarkNumberOrEmpty(self):
@@ -104,18 +112,15 @@ class BBox(RelativeLayout):
             self.clear_widgets()
             self.add_widget(self.blabel)
             self.isClear = True
-        
-            for i in range(self.row - 1, self.row + 1):
-                if i <= 0 or i > (self.root.level + Rows_For_First_Level):
-                    continue
-                for j in range(self.col - 1, self.col + 1):
-                    if j <= 0 or j > (self.root.level + Rows_For_First_Level):
-                        continue
-                    
-                    if i == self.row and j == self.col:
-                        continue
-                    
-                    self.root.BBoxList[j + (i-1)*(self.root.level + Rows_For_First_Level) - 1].MarkNumberOrEmpty()
+
+            self.root.Clear(self.row - 1,self.col - 1)
+            self.root.Clear(self.row - 1,self.col)
+            self.root.Clear(self.row - 1,self.col + 1)
+            self.root.Clear(self.row,self.col - 1)
+            self.root.Clear(self.row,self.col + 1)
+            self.root.Clear(self.row + 1,self.col - 1)
+            self.root.Clear(self.row + 1,self.col)
+            self.root.Clear(self.row + 1,self.col + 1)
                     
         elif self.BNumber > 0 and self.isClear == False:
             self.clear_widgets()
@@ -142,28 +147,92 @@ class FindBWidget(BoxLayout):
         self.status_bar.label_score.text = 'S:{}'.format(self.score)
 
     def Restart(self):
+        self.BBoxList = []
         self.gridSize = self.level + Rows_For_First_Level
         self.play_area.clear_widgets()
         self.play_area.cols = self.gridSize
         self.play_area.rows = self.gridSize
         
-        for i in range(1,self.gridSize + 1):
-            for j in range(1,self.gridSize + 1):
-                b = BBox(root=self,isBomb=(True if (Random().randint(0,self.gridSize+1)==0) else False))
+        for i in range(0,self.gridSize):
+            for j in range(0,self.gridSize):
+                b = BBox(root=self)
                 b.row=i
                 b.col=j
-                b.CalculateBNumber()
                 self.BBoxList.append(b)
                 self.play_area.add_widget(b)
         
+        self._calculate_bombs()
+        
+        self.status_bar.show_all.disable = False
+        self.status_bar.toggle_mark.disable = False
+                
+                
+    def _calculate_bombs(self):
+        if self.level < 6:
+            self.brate = 0.11
+        elif self.level < 11:
+            self.brate = 0.13
+        elif self.level < 20:
+            self.brate = 0.15
+        elif self.level < 30:
+            self.brate = 0.18
+        else:
+            self.brate = 0.2
+            
+        self.bnumber = int(self.brate * self.gridSize * self.gridSize)
+        
+        self.badded = 0
+        while True:
+            if self._add_bomb():
+                self.badded += 1
+                
+            if self.badded > self.bnumber:
+                break
+                
+        for i in range(0,len(self.BBoxList)):
+            self.BBoxList[i].CalculateBNumber()
+    
+    def _add_bomb(self):
+        i = Random().randint(0,len(self.BBoxList) - 1)
+        
+        if self.BBoxList[i].isBomb:
+            return False
+        else:
+            self.BBoxList[i].isBomb = True
+            print i
+            return True
+        
+    def CheckBomb(self,row,col):
+        if row < 0 or row >= self.gridSize or col < 0 or col >= self.gridSize:
+            return False
+        
+        index = row*self.gridSize + col
+        if index < 0 or index >= len(self.BBoxList):
+            return False
+        
+        return self.BBoxList[index].isBomb
+    
+    def Clear(self,row,col):
+        if row < 0 or row >= self.gridSize or col < 0 or col >= self.gridSize:
+            return
+        
+        index = row*self.gridSize + col
+        if index < 0 or index >= len(self.BBoxList):
+            return
+        
+        self.BBoxList[index].MarkNumberOrEmpty()
     
     def ShowAll(self):
         self.status_bar.toggle_mark.state='normal'
-        for i in range(1,len(self.BBoxList)):
-            self.BBoxList[i].bbutton.press()
+        for i in range(0,len(self.BBoxList)):
+            if self.BBoxList[i].isBomb:
+               self.BBoxList[i].bbutton.MarkB()
+            else:
+                self.BBoxList[i].Clear()
     
     def GameOver(self):
-        pass
+        self.status_bar.show_all.disable = True
+        self.status_bar.toggle_mark.disable = True
     
 class FindBApp(App): 
     def build(self):
